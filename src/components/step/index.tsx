ebd354d604a4f41ac358ce4e5753b56ef06dd131
Wilker V. Santos <wilker.vinicios@hotmail.com>
2024-12-15 00:47:19 -0300

    Adicionado icones as props e estilização.

--- src/components/step/index.tsx
+++ src/components/step/index.tsx
@@ -1,4 +1,5 @@
 import { View, Text } from "react-native";
+import { IconProps } from "@tabler/icons-react-native";
 
 import { colors } from "@/styles/colors";
 import { s } from "./styles";
@@ -6,11 +7,13 @@ import { s } from "./styles";
 type StepProps = {
     title: string;
     description: string;
+    icon: React.ComponentType<IconProps>;
 }
 
-export function Step({title, description}: StepProps) {
+export function Step({title, description, icon: Icon}: StepProps) {
     return (
       <View style={s.container}>
+        {Icon && <Icon size={24} color={colors.red.base} />}
         <View style={s.details}>
           <Text style={s.title}>{title}</Text>
           <Text style={s.description}>{description}</Text>
