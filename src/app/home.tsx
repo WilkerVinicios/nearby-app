aac0a9f22d825f0a164e4bd58fe99f801c53863d
Wilker V. Santos <wilker.vinicios@hotmail.com>
2024-12-15 21:26:01 -0300

    Adicionado componentes de categoria e regras da tela home.

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,7 +1,32 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, View } from "react-native";
+
+import { api } from "../servers/api";
+
+import { Categories, CategoriesProps } from "@/components/categories";
 
 export default function Home() {
+    const [categories, setCategories] = useState<CategoriesProps>([])
+    const [category, setCategory] = useState("")
+
+    async function fetchCategories() {
+      try {
+        const { data } = await api.get("/categories");
+        setCategories(data);
+        setCategory(data[0].id);
+      } catch (error) {
+        console.error(error);
+        Alert.alert("Categorias", "Erro ao buscar categorias");
+      }
+    }
+
+    useEffect(() => {
+        fetchCategories();
+    }, [])
+
     return (
-        <View style={{ flex: 1}}></View>
+        <View style={{ flex: 1}}>
+            <Categories data={categories} onSelect={setCategory} select={category} />
+        </View>
     )
 }
