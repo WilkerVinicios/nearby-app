6e58782fbea7b2d88dcdd79bde5ed4930d372406
Wilker V. Santos <wilker.vinicios@hotmail.com>
2025-01-05 17:53:53 -0300

    feat: Componentes de Place e Places estilizados com funcionamento de bottom sheet.

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -4,10 +4,15 @@ import { Alert, View } from "react-native";
 import { api } from "../servers/api";
 
 import { Categories, CategoriesProps } from "@/components/categories";
+import { PlaceProps } from "@/components/place";
+import { Places } from "@/components/places";
+
+type MarketProps = PlaceProps
 
 export default function Home() {
     const [categories, setCategories] = useState<CategoriesProps>([])
     const [category, setCategory] = useState("")
+    const [markets, setMarkets] = useState<MarketProps[]>([])
 
     async function fetchCategories() {
       try {
@@ -20,13 +25,37 @@ export default function Home() {
       }
     }
 
+    async function fetchMarkets() {
+      try {
+        if (!category) {
+          return;
+        }	
+
+        const { data } = await api.get("/markets/category/" + category);
+        setMarkets(data);
+      } catch (error) {
+        console.error(error);
+        Alert.alert("Locais", "Erro ao buscar locais.");
+      }
+    }
+
     useEffect(() => {
         fetchCategories();
     }, [])
 
+    useEffect(() => {
+        fetchMarkets();
+    }, [category])
+
     return (
         <View style={{ flex: 1}}>
-            <Categories data={categories} onSelect={setCategory} select={category} />
+            <Categories 
+              data={categories} 
+              onSelect={setCategory} 
+              select={category} 
+            />
+
+            <Places data={markets} />
         </View>
     )
 }
